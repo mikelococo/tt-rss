3b03184ce88b6e314555acb4723b3c8660862a3f
Mike Lococo <mikelococo@gmail.com>
2013-09-28 13:10:52 -0400

    Update config file to latest, and add a few extra attrs

--- attributes/default.rb
+++ attributes/default.rb
@@ -3,12 +3,15 @@
 # Recipe:: default
 #
 #
-default['tt-rss']['database']['name']     = 'tt-rss'
 default['tt-rss']['database']['user']     = 'tt-rss'
 default['tt-rss']['database']['password'] = 'tt-rss'
+default['tt-rss']['database']['name']     = 'tt-rss'
+default['tt-rss']['database']['host']     = 'localhost'
 
 default['tt-rss']['download-url'] = 'https://github.com/gothfox/Tiny-Tiny-RSS/archive/1.10.tar.gz'
 
 default['tt-rss']['server_name'] = "my-tinyrss.com"
 default['tt-rss']['url']         = "http://my-tinyrss.com"
 default['tt-rss']['install_dir'] = "/opt/tt-rss"
+
+default['tt-rss']['log_destination'] = 'sql'
