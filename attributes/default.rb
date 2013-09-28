10b6d9ac70909d39c205a2e4e06f970e7b4ca38f
Mike Lococo <mikelococo@gmail.com>
2013-09-28 12:26:07 -0400

    Update default to 1.10.0

--- attributes/default.rb
+++ attributes/default.rb
@@ -7,7 +7,7 @@ default['tt-rss']['database']['name']     = 'tt-rss'
 default['tt-rss']['database']['user']     = 'tt-rss'
 default['tt-rss']['database']['password'] = 'tt-rss'
 
-default['tt-rss']['download-url'] = "https://github.com/gothfox/Tiny-Tiny-RSS/archive/1.7.8.tar.gz"
+default['tt-rss']['download-url'] = 'https://github.com/gothfox/Tiny-Tiny-RSS/archive/1.10.tar.gz'
 
 default['tt-rss']['server_name'] = "my-tinyrss.com"
 default['tt-rss']['url']         = "http://my-tinyrss.com"
