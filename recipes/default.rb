a574789d60a0f7822e19e413ed23d9d699f9a31b
Mike Lococo <mikelococo@gmail.com>
2013-10-05 20:43:15 -0400

    Don't create the database twice.

--- recipes/default.rb
+++ recipes/default.rb
@@ -36,11 +36,6 @@ mysql_database database_name do
   action :create
 end
 
-mysql_database database_name do
-  connection ({:host => "localhost", :username => 'root', :password => node['mysql']['server_root_password']})
-  action :create
-end
-
 mysql_database_user database_user do
   connection ({:host => "localhost", :username => 'root', :password => node['mysql']['server_root_password']})
   password database_passsword
