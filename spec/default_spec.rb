1117b3dc5aa650fc3aca70cbff22ef7b84950faa
Mike Lococo <mikelococo@gmail.com>
2014-08-10 15:46:17 -0400

    Get chefspec tests passing again.

--- spec/default_spec.rb
+++ spec/default_spec.rb
@@ -1,4 +1,9 @@
 require 'chefspec'
+require 'chefspec/berkshelf'
+require 'chefspec/deprecations'
+
+# Generate a code-coverage report, handy for finding new tests to write.
+# at_exit { ChefSpec::Coverage.report! }
 
 describe 'tt-rss' do
   let(:chef_run) do
@@ -10,7 +15,6 @@ describe 'tt-rss' do
     runner.node.set['mysql']['server_repl_password']   = 'test_pw'
     runner.node.set['mysql']['server_debian_password'] = 'test_pw'
     runner.converge 'tt-rss::default'
-    runner
   end
   it 'installs php-apc' do
     expect(chef_run).to install_package 'php-apc'
@@ -28,19 +32,21 @@ describe 'tt-rss' do
     expect(chef_run)
       .to create_directory "#{chef_run.node['tt-rss']['install_dir']}"
   end
+
   it 'downloads tt-rss' do
-    let(:filename) { "#{chef_run.node['tt-rss']['install_dir']}/tt-rss.tar.gz" }
-    expect(chef_run).to create_remote_file :filename
+    ttrss_tarball = "#{chef_run.node['tt-rss']['install_dir']}/tt-rss.tar.gz"
+    expect(chef_run).to create_remote_file ttrss_tarball
   end
   it 'creates the tt-rss config.php' do
-    let(:config_file) { "#{chef_run.node['tt-rss']['install_dir']}/config.php" }
-    expect(chef_run).to create_file :config_file
+    config_file = "#{chef_run.node['tt-rss']['install_dir']}/config.php"
+    expect(chef_run).to create_template config_file
     # rubocop:disable Style/LineLength
     # expect(chef_run).to create_file_with_content "#{chef_run.node['tt-rss']['install_dir']}/config.php", 'define'
     # rubocop:enable Style/LineLength
   end
   it 'creates the update_feeds cronjob' do
     expect(chef_run)
-      .to create_file_with_content '/etc/cron.d/ttrss-update', '/usr/bin/php'
+      .to render_file('/etc/cron.d/ttrss-update')
+      .with_content('/usr/bin/php')
   end
 end
