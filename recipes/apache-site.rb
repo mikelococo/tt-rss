9a3d054a9e65bb8c36a489889f22989182d1a60b
Mike Lococo <mikelococo@gmail.com>
2014-08-10 10:11:09 -0400

    Rubocop style compliance

--- recipes/apache-site.rb
+++ recipes/apache-site.rb
@@ -1,15 +1,13 @@
+# rubocop:disable Style/FileName
 #
 # Cookbook Name:: tt-rss
 # Recipe:: apache-site
-#
-#
 
 install_dir = node['tt-rss']['install_dir']
 
 # install web app in apache
-web_app "tt-rss" do
+web_app 'tt-rss' do
   server_name node['tt-rss']['server_name']
   server_aliases []
   docroot install_dir
 end
-
--- spec/default_spec.rb
+++ spec/default_spec.rb
@@ -1,8 +1,8 @@
 require 'chefspec'
 
 describe 'tt-rss' do
-  let (:chef_run) {
-    runner = ChefSpec::ChefRunner.new(platform:'ubuntu', version:'12.04', :log_level => :debug)
+  let(:chef_run) do
+    runner = ChefSpec::Runner.new(platform: 'ubuntu', version: '12.04')
     # When running under chef-solo (or chefspec), the mysql cookbook checks
     # for these attributes and raising a Chef::Application.fatal if they're
     # missing.
@@ -11,7 +11,7 @@ describe 'tt-rss' do
     runner.node.set['mysql']['server_debian_password'] = 'test_pw'
     runner.converge 'tt-rss::default'
     runner
-  }
+  end
   it 'installs php-apc' do
     expect(chef_run).to install_package 'php-apc'
   end
@@ -25,16 +25,22 @@ describe 'tt-rss' do
     expect(chef_run).to install_package 'php5-mcrypt'
   end
   it 'creates the tt-rss install dir' do
-    expect(chef_run).to create_directory "#{chef_run.node['tt-rss']['install_dir']}"
+    expect(chef_run)
+      .to create_directory "#{chef_run.node['tt-rss']['install_dir']}"
   end
   it 'downloads tt-rss' do
-    expect(chef_run).to create_remote_file "#{chef_run.node['tt-rss']['install_dir']}/tt-rss.tar.gz"
+    let(:filename) { "#{chef_run.node['tt-rss']['install_dir']}/tt-rss.tar.gz" }
+    expect(chef_run).to create_remote_file :filename
   end
   it 'creates the tt-rss config.php' do
-    expect(chef_run).to create_file "#{chef_run.node['tt-rss']['install_dir']}/config.php"
-    #expect(chef_run).to create_file_with_content "#{chef_run.node['tt-rss']['install_dir']}/config.php", 'define'
+    let(:config_file) { "#{chef_run.node['tt-rss']['install_dir']}/config.php" }
+    expect(chef_run).to create_file :config_file
+    # rubocop:disable Style/LineLength
+    # expect(chef_run).to create_file_with_content "#{chef_run.node['tt-rss']['install_dir']}/config.php", 'define'
+    # rubocop:enable Style/LineLength
   end
   it 'creates the update_feeds cronjob' do
-    expect(chef_run).to create_file_with_content '/etc/cron.d/ttrss-update', '/usr/bin/php'
+    expect(chef_run)
+      .to create_file_with_content '/etc/cron.d/ttrss-update', '/usr/bin/php'
   end
 end
